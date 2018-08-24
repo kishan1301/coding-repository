c6c52b137cf1f48a1cdd58b1211b7220fc902063
Kishan Gupta <kishangupta1301@gmail.com>
2018-08-24 22:56:18 +0530

    Update Madlibs.py

--- Madlibs.py
+++ Madlibs.py
@@ -1,3 +1,5 @@
+"""This is the Madlibs game."""
+
 STORY = "This morning %s woke up feeling %s. 'It is going to be a %s day!' Outside, a bunch of %ss were protesting to keep %s in stores. They began to %s to the rhythm of the %s, which made all the %ss very %s. Concerned, %s texted %s, who flew %s to %s and dropped %s in a puddle of frozen %s. %s woke up in the year %s, in a world where %ss ruled the world."
 
 print("The Mad Libs has started!!")
