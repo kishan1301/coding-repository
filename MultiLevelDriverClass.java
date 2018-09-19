183f205281249dc41cc752f690d8ee725b417576
Kishan Gupta <kishangupta1301@gmail.com>
2018-09-19 17:43:23 +0530

    Add files via upload

--- MultiLevelDriverClass.java
+++ MultiLevelDriverClass.java
@@ -14,35 +14,78 @@ public class MultiLevelDriverClass {
 		 * System.out.println(employee instanceof Employee);
 		 * System.out.println(employee2 instanceof Manager);
 		 */
-		boolean loop = true;
+		/// boolean loop = true;
 
-		while (loop = true) {
-			//System.out.println("NSEL...");
-			//System.out.println("1.");
-			
-			
-			//Creating the employees
-			System.out.println("Enter the number of employees: ");
-			int size = in.nextInt();
-			
-			Employee[] employees = new Employee[size];
-			
-			for(int i = 0; i < size; i++)	{
-				
+		// while (loop) {
+		// System.out.println("NSEL...");
+		// System.out.println("1.");
+
+		// Creating the employees
+		System.out.println("Enter the number of employees: ");
+		int size = in.nextInt();
+
+		Employee[] employees = new Employee[size];
+
+		for (int i = 0; i < size; i++) {
+			System.out.println("Enter the employee ID: ");
+			employees[i] = createEmp(in.nextInt());
+
+		}
+
+		/// loop = false;
+		// }
+
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			if (employees[i].getDesignation().equalsIgnoreCase("manager")) {
+				count++;
 			}
-			
-			loop = false;
+		}
+
+		System.out.println("The number of manager(s) are " + count);
+
+		System.out.println("Number of departments are " + Employee.getNumOfDept());
+		System.out.println("List of departments are " + Employee.numOfDept);
+
+		float maxSalary = employees[0].getSalary();
+		int mark = 0;
+		for (int i = 0; i < size; i++) {
+
+			if (employees[i].getSalary() > maxSalary) {
+				maxSalary = employees[i].getSalary();
+				mark = i;
+			}
+		}
+
+		System.out.println("The maximum salary is the of employee " + employees[mark].getUserName()
+				+ " and the salary is " + maxSalary);
+
+		// ending the max salary query
+
+		// initiating the joining date of the employee query
+		System.out.print("Enter the Employee's name to search for...");
+		String searchName = in.next();
+
+		for (int i = 0; i < size; i++) {
+			if (employees[i].getUserName().equalsIgnoreCase(searchName)) {
+				System.out
+						.println("The joining date of the employee" + searchName + " is " + employees[i].getHireDate());
+			}
+		}
+		// ending of the joining date of the employee query
+
+		
+		// initiating the string containing "s" in the employee name query
+		System.out.println("Name(s) of the employees ontaininf the aplhabet 's' are...");
+		for (int i = 0; i < size; i++) {
+			if (employees[i].getUserName().contains("s")) {
+				System.out.print(employees[i].getUserName()+ " ");
+			}
 		}
 		
-		
-		
-		
-		
-		
-		
 	}
-	
-	public static Employee createEmp(int id)	{
+
+	public static Employee createEmp(int id) {
 		return new Employee(id);
 	}
 
